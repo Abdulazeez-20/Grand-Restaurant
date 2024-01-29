e30ea23df088a5865fa0ab9850182c8febd55c7f
Abdulazeez_20@yahoo.com <85404458+Abdulazeez-20@users.noreply.github.com>
2024-01-29 22:27:47 +0300

    added the src structure

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { GitHamburgerMenu } from 'react-icons/gi'
+import { MdOutLineRestaurantMenu } from 'react-icons/md'
+
+import './Navbar.css';
+
+const Navbar = () => (
+  <div className='app__navbar'>
+
+  </div>
+);
+
+export default Navbar;
