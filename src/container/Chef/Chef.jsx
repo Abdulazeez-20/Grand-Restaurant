072743e14483848b7c9e6258b5d35273a1133a91
Abdulazeez_20@yahoo.com <85404458+Abdulazeez-20@users.noreply.github.com>
2024-02-07 22:24:14 +0300

    added the html of the Chef's section

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants'
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef img" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's word" />
+      <h1 className='headtext__cormorant'>What We Believe In</h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote" />
+          <p className='p__opensans'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed minima sint deserunt voluptates vitae quibusdam rem, perspiciatis cumque atque, accusamus velit dolor voluptatibus corrupti culpa, dolorum mollitia at accusantium nobis.  perspiciatis cumque atque, accusamus velit dolor voluptatibus corrupti culpa</p>
+        </div>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Gordan Sani</p>
+        <p className='p__opensans'>Chef's Signeture</p>
+        <img src={images.sign} alt="chef's signeture" />
+      </div>
+    </div>
   </div>
 );
 
