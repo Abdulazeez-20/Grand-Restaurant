d8c12a08074009e65facc138db903534ba064294
Abdulazeez_20@yahoo.com <85404458+Abdulazeez-20@users.noreply.github.com>
2024-02-17 23:29:52 +0300

    Gallery container done

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -10,30 +10,30 @@ const gallery_images = [images.gallery01, images.gallery02, images.gallery03, im
 const Gallery = () => {
   const scrollRef = React.useRef(null)
   const scroll = (direction) => {
-    const { curretn } = scrollRef;
+    const { current } = scrollRef;
 
     if (direction === 'left') {
-      curretn.scrollLeft -= 300;
+      current.scrollLeft -= 300;
     } else {
-      curretn.scrollRight += 300;
+      current.scrollLeft += 300;
     }
   }
 
   return (
     <div className='gallery flex__center'>
-      <div className="gallery_content">
+      <div className="gallery-content">
         <SubHeading title="Instagram" />
         <h1 className='headtext__cormorant'>Photo Gallery</h1>
         <p className='p__opensans' style={{ color: '#AAA', marginTop: '2rem' }}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum dicta reiciendis pariatur, neque minima ipsa doloribus. Et, animi autem aspernatur odio amet fugit tempore fuga esse maxime repudiandae ipsa omnis.</p>
         <button type='button' className='custom__button'>View More</button>
       </div>
 
-      <div className="gallery_images">
+      <div className="gallery-images">
         <div className="gallery-images_container" ref={scrollRef}>
           {gallery_images.map((image, index) => (
             <div className="gallery-images_card flex__center" key={`gallery_image-${index + 1}`}>
               <img src={image} alt="Gallery" />
-              <BsInstagram className='gallery_image-icon' />
+              <BsInstagram className='gallery-image_icon' />
             </div>
           ))}
         </div>
