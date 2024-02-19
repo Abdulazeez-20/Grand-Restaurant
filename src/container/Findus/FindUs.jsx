d9073fb5d987cb6dc6c68b12bd66457f1f6dbd85
Aziz <Abdulazeez_20@yahoo.com>
2024-02-19 22:21:37 +0300

    Find US container done

--- src/container/Findus/FindUs.jsx
+++ src/container/Findus/FindUs.jsx
@@ -5,7 +5,23 @@ import { images } from '../../constants';
 
 const FindUs = () => (
   <div className='app__bg app__wrapper section__padding' id="contact">
+    <div className="app__wrapper_info">
+      <SubHeading title="Contact" />
+      <h1 className='headtext__cormorant' style={{ marginBottom: "3rem" }}>Find Us</h1>
 
+      <div className='app__wrapper-info_content'>
+        <p className='p__opensans'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa </p>
+        <p className='p__cormorant' style={{ color: "#DCCA87", margin: "2rem 0" }}>Opening Hours</p>
+        <p className='p__opensans'>Sun - Fri: 10:00 am - 02:00 pm</p>
+        <p className='p__opensans'>Sun - Fri: 10:00 am - 02:00 pm </p>
+      </div>
+
+      <button type='button' className='custom__button' style={{ marginTop: "2rem" }}>Visit Us</button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.findus} alt="Find Us" />
+    </div>
   </div>
 );
 
